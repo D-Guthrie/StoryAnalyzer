6fcacbd54f70aa49a71275e3de1036f3cad1d581
DSGuthrie <david.s.guthrie@gmail.com>
2019-08-08 02:38:37 -0400

    Updated imports, started creating NLP properties

--- src/languageAnalysis/WordClassifier.java
+++ src/languageAnalysis/WordClassifier.java
@@ -1,6 +1,22 @@
 package languageAnalysis;
 
+import java.util.Properties;
+
+import edu.stanford.nlp.io.*;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.*;
+import edu.stanford.nlp.util.*;
+
+
 public class WordClassifier {
 	
 	// TODO: integrate Stanford CoreNLP
+	
+	Properties props = new Properties();
+	
+	StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+	
+	
 }
